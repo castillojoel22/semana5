5453db0576a58d61a0c1643e66fdfea5d67a7b8d
Joel Castillo <castillocaballeroj@iestptrujillo.net>
2024-09-19 11:01:52 -0500

    Mi primer Commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("DevolverAlumno")]
+        public string DevolverAlumno()
+        {
+            string mensaje = string.Empty;
+            Alumno Alumno1 = new Alumno();
+            Alumno Alumno2 = new Alumno("Juan", "Perez", "4370258");
+            return Alumno2.DevolverNombreCompleto();
+        }
+
+        [HttpGet("DevolverDocumento")]
+        public string DevolverDocumento()
+        {
+            string mensaje = string.Empty;
+            Alumno Alumno1 = new Alumno();
+            Alumno Alumno2 = new Alumno("Juan", "Perez", "4370258");
+            return Alumno2.DevolverDocumento();
+        }
+
+        [HttpGet("DevolverEdad")]
+        public int DevolverEdad()
+        {
+            string mensaje = string.Empty;
+            Alumno Alumno3 = new Alumno("Joel", "Castillo", "4370258", new DateTime(2003, 8, 02));
+            return Alumno3.DevolverEdad();
+        }
+
+    }
+}
